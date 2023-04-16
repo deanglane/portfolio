c0131c90ad436f101b11c8d3723624d6e0d4f5d7
deanglane <dglaneto@gmail.com>
2023-04-16 19:34:32 -0400

    styled portfolio cards

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ app.portfolio = [
     url: "https://deanlane-proj01-bar-one.netlify.app/",
     desc: "Web site for a bar restaurant that features a responsive design, blog and a vanilla javascript meal ordering system  ",
     tag: ["website", "feature"],
-    icon: "<i class=\"fa- solid fa - laptop - code\"></i>",
+    icon: "fa-solid fa-laptop-code",
     languages: [
       "./assets/svG/html5-original-wordmark.svg",
       "./assets/svg/css3-original-wordmark.svg",
@@ -19,56 +19,56 @@ app.portfolio = [
     url: "http",
     desc: "something",
     tag: "website",
-    icon: "<i class=\"fa- solid fa - laptop - code\"></i>"
+    icon: "fa-solid fa-laptop-code"
   },
   {
     name: "web3",
     url: "http",
     desc: "something",
     tag: "website",
-    icon: "<i class=\"fa- solid fa - laptop - code\"></i>"
+    icon: "fa-solid fa-laptop-code"
   },
   {
     name: "web4",
     url: "http",
     desc: "something",
     tag: "website",
-    icon: "<i class=\"fa- solid fa - laptop - code\"></i>"
+    icon: "fa-solid fa-laptop-code"
   },
   {
     name: "web5",
     url: "http",
     desc: "something",
     tag: "website",
-    icon: "<i class=\"fa- solid fa - laptop - code\"></i>"
+    icon: "fa-solid fa-laptop-code"
   },
   {
     name: "web6",
     url: "http",
     desc: "something",
     tag: "website",
-    icon: "<i class=\"fa- solid fa - laptop - code\"></i>"
+    icon: "fa-solid fa-laptop-code"
   },
   {
     name: "web7",
     url: "http",
     desc: "something",
     tag: "website",
-    icon: "<i class=\"fa- solid fa - laptop - code\"></i>"
+    icon: "fa-solid fa-laptop-code"
   },
   {
     name: "Pokemon Pokedex app",
     url: "https://dean-lane-pokemon-app.netlify.app/",
     desc: "A pokemon themed app demonstrating an Rest API call",
     tag: ["app", "feature"],
-    icon: "<i class=\"fa- brands fa - app - store\"></i>"
+    icon: "fa-brands fa-app-store"
   },
   {
     name: "app2",
     url: "http",
     desc: "something",
     tag: "app",
-    icon: "<i class=\"fa- brands fa - app - store\"></i>"
+    icon: "fa-brands fa-app-store"
   }
 ];
 
@@ -97,7 +97,8 @@ app.portfolioLoad = (value) => {
   cards.forEach((card)=>{
     const portfolioLi = document.createElement('li');
     portfolioLi.innerHTML = 
-    `<h4>${card.name}</h4>
+    `<i class="${card.icon}"></i>
+    <h4>${card.name}</h4>
     <div class="portfolioPreviewImg">
       <img src="https://placekitten.com/250/250" alt="">
     </div>
@@ -107,9 +108,8 @@ app.portfolioLoad = (value) => {
       <img src="./assets/svg/sass-original.svg" alt="">
       <img src="./assets/svg/javascript-original.svg" alt="">
     </div>
-    <a href="${card.url}">Live Preview</a>`
-    
-    ;
+    <a href="${card.url}">Live Preview</a>`;
+
     portfolioLi.classList.add('portfolioCard');
     portfolioUl.appendChild(portfolioLi);
   })
